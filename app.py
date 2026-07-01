f3371bfafd68c4295a4eefdcd6dd32863b113dc4
Laali Nembot <laalinembot@gmail.com>
2026-07-01 21:54:03 +0000

    added echo display

--- app.py
+++ app.py
@@ -19,5 +19,17 @@ def register():
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/lnembot/New_Flask')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
